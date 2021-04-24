09686d795d986c2592641325535d65ab9b8388c5
g1d5ny <jiwon936626@naver.com>
2021-04-24 21:14:23 +0900

    heroku update

--- photo/views.py
+++ photo/views.py
@@ -3,14 +3,36 @@ from .models import Photo
 from django.shortcuts import render, redirect
 from .models import Photo
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-@login_required
 def photo_list(request):
     photos = Photo.objects.all()
     return render(request, 'photo/list.html', {'photos': photos})
+
+# class PhotoListView(ListView):
+#     # model = Photo
+#     context_object_name = 'photo_list'
+#     # queryset = Photo.objects.all()
+#     template_name = 'photo/list.html'
+#
+#     def get_queryset(self, request,*args, **kwargs):
+#         queryset = Photo.objects.all()
+#         return queryset
+
+
+# class PhotoDetailView(DetailView):
+#     template_name = "photo/detail.html"
+#     model = Photo
+#     context_object_name = "photo"
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['form'] = OrderForm(self.request)
+#         return context
 
 
 class PhotoUploadView(CreateView):
